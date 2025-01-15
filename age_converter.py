92f66ce1e8f837530d156473b0bb89716b060c9a
prakash <bhandariprakash6548@gmail.com>
2025-01-15 19:17:49 +0545

    Removed scoreapp.py from being tracked

--- age_converter.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import datetime
-from age_operations import age_converter
-d1= st.date_input("Enter your date of birth",format="YYYY/MM/DD")
-d2=st.date_input("Today's Date",format="YYYY/MM/DD")
-if st.button("convert"):
- your_age=age_converter(d1, d2)
- st.status(f"your age is{your_age}",expanded=False,state="complete")
